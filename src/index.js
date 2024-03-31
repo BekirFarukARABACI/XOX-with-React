f43b737ce41587302abd105a5cf05caa7317f223
Bekir Faruk ARABACI <113997401+BekirFarukARABACI@users.noreply.github.com>
2024-03-31 17:07:27 +0300

    feat : Game Board Added

--- src/index.js
+++ src/index.js
@@ -1,17 +1,60 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
+import './index.css'
 
 
-function XoxGameComponent(){
-  return(
+function XoxGameComponent() {
+  const [games, setGames] = useState([]);
+  const [mark, setMark] = useState("X");
+  const [message, setMessage] = useState("");
+  const [isGameFinish, setIsGameFinish] = useState(false);
+
+  useEffect(() => {
+    newGame()
+  }, [])
+
+  const newGame = () => {
+    setGames([
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      ""
+    ]);
+
+    setIsGameFinish(false);
+    setMark("X")
+    setMessage("Hamle Sırası : " + mark)
+  }
+
+  return (
     <>
-      <h1>XOX Game</h1>
+      <div className='container text-center'>
+        <h1 className='alert'>XOX Game</h1>
+        <h2 className='alert alert-warning'>
+          {message}
+        </h2>
+        <button className='btn btn-outline-primary w-100'>
+          Yeni Oyun
+        </button>
+        <div className='row mt-2'>
+          {games.map((game, index) => (
+            <div key={index} className='col-4 box'>
+              {game}
+            </div>
+          ))}
+        </div>
+      </div>
     </>
   )
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<XoxGameComponent/>);
+root.render(<XoxGameComponent />);
 
 reportWebVitals();
