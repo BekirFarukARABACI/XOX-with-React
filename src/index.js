5ad58434f9635817f0f31950e2c2d1a478736ac1
Bekir Faruk ARABACI <113997401+BekirFarukARABACI@users.noreply.github.com>
2024-03-31 16:52:26 +0300

    feat: Create Project

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import reportWebVitals from './reportWebVitals';
+
+
+function XoxGameComponent(){
+  return(
+    <>
+      <h1>XOX Game</h1>
+    </>
+  )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<XoxGameComponent/>);
+
+reportWebVitals();
