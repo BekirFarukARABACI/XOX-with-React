13c6bfb1f5e02fa37c3e068b46a65aa584b46122
Bekir Faruk ARABACI <113997401+BekirFarukARABACI@users.noreply.github.com>
2024-03-31 17:30:38 +0300

    Feat : Winner Check Added

--- src/index.js
+++ src/index.js
@@ -46,11 +46,66 @@ function XoxGameComponent() {
           return
         }
 
+        let r = isGameOver(newGames);
+        if(r){
+          setMessage("Oyunun Kazananı : " + mark)
+          setIsGameFinish(true)
+          return
+        }
+
         mark == "X" ? setMark("O") : setMark("X")
         setMessage("Hamle Sırası : " + (mark == "X" ? "O" : "X"))
       }
     }
   }
+
+  const isGameOver = (newGames) =>{
+    if(newGames[0] != "" && 
+    newGames[0] === newGames[1] && 
+    newGames[0] === newGames[2]){
+      return true
+    }
+    if(newGames[3] != "" && 
+    newGames[3] === newGames[4] && 
+    newGames[3] === newGames[5]){
+      return true
+    }
+    if(newGames[6] != "" && 
+    newGames[6] === newGames[7] && 
+    newGames[6] === newGames[8]){
+      return true
+    }
+
+    if(newGames[0] != "" && 
+    newGames[0] === newGames[3] && 
+    newGames[0] === newGames[6]){
+      return true
+    }
+    if(newGames[1] != "" && 
+    newGames[1] === newGames[4] && 
+    newGames[1] === newGames[7]){
+      return true
+    }
+    if(newGames[2] != "" && 
+    newGames[2] === newGames[5] && 
+    newGames[2] === newGames[8]){
+      return true
+    }
+
+    if(newGames[0] != "" && 
+    newGames[0] === newGames[4] && 
+    newGames[0] === newGames[8]){
+      return true
+    }
+    if(newGames[2] != "" && 
+    newGames[2] === newGames[4] && 
+    newGames[2] === newGames[6]){
+      return true
+    }
+
+    return false
+  }
+
 
   function isMoveFinish(newGames){
     for (let i = 0; i < newGames.length; i++) {
